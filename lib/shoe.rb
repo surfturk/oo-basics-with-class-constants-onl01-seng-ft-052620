c7effb71e10d695d408f9c67cc5acd845de32de1
Berkley Brown <berkleydeane@gmail.com>
2020-06-08 19:11:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,14 +7,6 @@ class Shoe
  BRANDS = []
   
   
-  def initialize(brand)
-    @brand = brand
-    BRANDS << @brand
-    
- end 
-  
-  
-  
   def initialize(brand)    
        @brand = brand     
       if !(BRANDS.include?(@brand))     
